25e92c743a78ad9a74bb619e8e6a254022dc8d1f
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-06 13:39:27 -0500

    js1

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 const tablanota = {
-   tradicional : ["90-100","80-89","70-79","60-69",">60","0"],
-   pointrange: ["12-14","9-11","6-8","3-5","1-2","0"],
-   letter : ["A","B","C","D","E","Z"],
+   Tradicional : ["90-100","80-89","70-79","60-69","<60","0"],
+   Rango: ["12-14","9-11","6-8","3-5","1-2","0"],
+   Letras : ["A","B","C","D","E","F"],
    SBG: [4,3,2,1,0,0],
 };
-console.table([tablanota]);
+console.table(tablanota);
 
-let nota = {};
-nota= prompt("¿Cuál es tu nota?");
+let nota ={};
+let metodo = 0;
+metodo=prompt("Escoge tu tipo de calificación (1-3): 1.Tradicional 2.Point-Range 3.Letras");
+   if (metodo == 1){
+      console.log("Haz escogido el Metodo Tradicional")
+      nota= prompt("¿Cuál es tu nota? (%)")
+      switch(nota){
+         case (nota>90&&nota<100):
+            mensaje = "super";
+            break;
+         case (nota>80&&nota<89):
+            mensaje = "bien";
+            break;
+         case (nota>70&&nota<79):
+            mensaje = "p";
+            break;
+         case (nota>60&&nota<69):
+            mensaje = "a";
+            break;
+         case (nota<60):
+            mensaje = "y";
+            break;
+      }
+   } else if (metodo == 2){
+      console.log("Haz escogido el Metodo de Rango")
+      nota= prompt("¿Cuál es tu nota? (0-14)")
+   } else if (metodo == 3){
+      console.log("Haz escogido el Metodo de Letras")
+      nota= prompt("¿Cuál es tu nota? (A-F)")
+   } else{
+      alert("Elige nuevamente")
+   }
+
+console.log("Tu nota es " +nota+" "+mensaje);
