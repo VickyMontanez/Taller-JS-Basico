305937f7380c3f66500b04b288d4566775697aab
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-06 09:31:30 -0500

    js

---
+++ main.js
@@ -0,0 +1,10 @@
+const tablanota = {
+   tradicional : ["90-100","80-89","70-79","60-69",">60","0"],
+   pointrange: ["12-14","9-11","6-8","3-5","1-2","0"],
+   letter : ["A","B","C","D","E","Z"],
+   SBG: [4,3,2,1,0,0],
+};
+console.table([tablanota]);
+
+let nota = {};
+nota= prompt("¿Cuál es tu nota?");
